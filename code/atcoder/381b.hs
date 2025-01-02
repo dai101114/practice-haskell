5f1ef87dce199fdcca62c2dabe9f941cf73e47e0
dai101114 <swan.101114@gmail.com>
2025-01-02 15:08:11 +0900

    381b

---
+++ code/atcoder/381b.hs
@@ -0,0 +1,11 @@
+main :: IO ()
+main = do
+  s <- getLine
+  putStrLn $ if solve s [] then "Yes" else "No"
+
+solve :: String -> String -> Bool
+solve "" _ = True
+solve [_] _ = False 
+solve (s1:s2:s) xs | len `mod` 2 == 0 = s1 == s2 && not (s1 `elem` xs) && solve s (s1:xs)
+                   | otherwise = False
+                   where len = length s
